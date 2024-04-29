ae4e3ef53a53aa308413621a05bbbdf109666028
Savvas22K <159148057+Savvas22K@users.noreply.github.com>
2024-04-29 13:29:23 +0100

    Update FCP_assignment_v3.py

--- FCP_assignment_v3.py
+++ FCP_assignment_v3.py
@@ -20,8 +20,95 @@ class Network:
             self.nodes = nodes 
 
 	def get_mean_degree(self):
-		#Your code  for task 3 goes here
+		# Your code  for task 3 goes here
+		degree_list = []
+		# finds the total number of degrees across all nodes
+		for node in self.nodes:
+			degree_list.append(sum(node.connections))
+		degree_list = [sum(node.connections) for node in self.nodes]
+		# divides total degrees by number of nodes for mean
+		return sum(degree_list) / len(degree_list)
+	
+	def get_mean_clustering(self):
+		# Your code for task 3 goes here
+		# makes a list containing the clustering coefficient of each node
+		coefficients_list = []
+		for node in self.nodes:
+			# checks neighbours of each connection
+			neighbors = [self.nodes[i] for i, connection in enumerate(node.connections) if connection == 1]
+			# to skip checking when there cannot be a connection
+			if len(neighbors) < 2:
+				coefficients_list.append(0)
+				continue
+			node_connections = 0
+			# ckecks for neighbour connections for all nodes
+			for var1, neighbor1 in enumerate(neighbors):
+				for var2, neighbor2 in enumerate(neighbors):
+					# to avoid checking a pair twice
+					if var1 < var2 and neighbor1.connections[neighbor2.index] == 1:
+						# counts connections between neighbours
+						node_connections += 1
+			possible_connections = len(neighbors) * (len(neighbors) - 1) / 2
+			# adds clustering coefficient of each node to the list
+			coefficients_list.append(node_connections / possible_connections)
+		# finds sum of the list and divides by number of nodes in list for mean
+		return sum(coefficients_list) / len(coefficients_list)
 
+
+def get_num_connected_nodes(self, node):
+    # list where 0 = unvisited and 1 = visited
+    visited = [0] * len(self.nodes)
+    visited_list = [node]
+    # to mark as visited
+    visited[node.index] = 1
+    node_count = 0
+
+    while visited_list:
+        current_node = visited_list.pop(0)
+        node_count += 1
+        for i, connection in enumerate(current_node.connections):
+            if connection == 1 and not visited[i]:
+                visited[i] = 1
+                visited_list.append(self.nodes[i])
+    # checks if node has no connections
+    if node_count == 1:
+        return node_count
+    # -1 to subtract original node
+    return node_count - 1
+
+
+def shortest_path_length(self, start_node):
+    # list where 0 = unvisited and 1 = visited
+    visited = [0] * len(self.nodes)
+    # to set number of edges as distance
+    distance = [0] * len(self.nodes)
+    visited_list = [start_node]
+    # to set as  visited
+    visited[start_node.index] = 1
+
+    while visited_list:
+        current_node = visited_list.pop(0)
+        # loop to check for all connections
+        for i, connection in enumerate(current_node.connections):
+            if connection == 1 and not visited[i]:
+                visited[i] = 1
+                # adds 1 to the distance
+                distance[i] = distance[current_node.index] + 1
+                # adds node to visited list
+                visited_list.append(self.nodes[i])
+    return sum(distance)
+
+
+def get_mean_path_length(self):
+    # Your code for task 3 goes here
+    path_lenths_list = []
+    total_shortest_paths = 0
+    for start_node in self.nodes:
+        total_shortest_paths += self.shortest_path_length(start_node)
+        # adds shortest distance divided by number of connections to the list
+        path_lenths_list.append(self.shortest_path_length(start_node) / self.get_num_connected_nodes(start_node))
+    # divides total distance by number of nodes for mean
+    return sum(path_lenths_list) / len(self.nodes)
 	def get_mean_clustering(self):
 		#Your code for task 3 goes here
 
@@ -295,6 +382,42 @@ def main():
         ising_main(population, alpha, external)
     if args.test_ising:
         test_ising()
+	    parser = argparse.ArgumentParser()
+
+
+	parser.add_argument("-network", type=int)
+	parser.add_argument("-test_network", action='store_true')
+	
+	args = parser.parse_args()
+	network_size = args.network
+
+	if args.test_network:
+	    nodes = []
+	    num_nodes = 10
+	    for node_number in range(num_nodes):
+	        connections = [1 for val in range(num_nodes)]
+	        connections[node_number] = 0
+	        new_node = Node(0, node_number, connections=connections)
+	        nodes.append(new_node)
+	    network = Network(nodes)
+	
+	    assert (network.get_mean_degree() == num_nodes - 1), network.get_mean_degree()
+	    assert (network.get_mean_clustering() == 1), network.get_mean_clustering()
+	    assert (network.get_mean_path_length() == 1), network.get_mean_path_length()
+	    print("test")
+	
+	if args.network:
+	    network = Network()
+	    network.make_random_network(network_size)
+	
+	    mean_degree = network.get_mean_degree()
+	    mean_clustering = network.get_mean_clustering()
+	    mean_path_length = network.get_mean_path_length()
+	
+	    print("Mean Degree:", mean_degree)
+	    print("Mean Clustering Coefficient:", mean_clustering)
+	    print("Mean Path Length:", mean_path_length)
+	    network.plot()
 
 if __name__=="__main__":
 	main()
