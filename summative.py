7291c9246e22aa8320bc52724f43ba2f4266fc22
ik23450 <159148386+ik23450@users.noreply.github.com>
2024-05-01 13:42:54 +0100

    Update summative.py

--- summative.py
+++ summative.py
@@ -62,7 +62,7 @@ def ising_step(population, external=0.0, alpha=1.0):
     col = np.random.randint(0, n_cols)
 
     # Calculates agreement for selected point
-    agreement = calculate_agreement(population, row, col, external=None)
+    agreement = calculate_agreement(population, row, col, external=0.0)
     # Flips the person's opinion if negative agreement.
     # If a non-zero value of alpha is given, the probability a flip occurs is calculated.
     # If the probability of this is greater than a randomly generated float between 0.0 and 1.0 a flip of opinion occurs.
