b107d464e4b40db1106ab78ff29286c33572ee18
ik23450 <159148386+ik23450@users.noreply.github.com>
2024-05-01 18:46:30 +0100

    Update FINAL_FCP_assignment.py

--- FINAL_FCP_assignment.py
+++ FINAL_FCP_assignment.py
@@ -350,7 +350,7 @@ def ising_step(population, external=0.0, alpha=1.0):
     if agreement <= 0:
         population[row, col] *= -1
     else:
-	if alpha != 0:
+	    if alpha != 0:
 		random_prob = random.random()
 		if random_prob < math.e ** (-agreement / alpha):
 			population[row, col] *= -1
